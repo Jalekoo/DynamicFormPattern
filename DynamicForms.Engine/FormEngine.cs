05e8d7e08fb35e75f08b0279b6bc9e557d483d8f
Julien Mathon <julien.mathon@invenietis.com>
2013-07-12 11:52:31 +0200

    Serialize/Deserialize a form (non-test)

--- DynamicForms.Engine/FormEngine.cs
+++ DynamicForms.Engine/FormEngine.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,12 +18,20 @@ namespace DynamicForms.Engine
 
         public static void Save( string path, Form form )
         {
-            throw new NotImplementedException();
+            IFormatter formatter = new BinaryFormatter();
+            using( FileStream bin = new FileStream( Path.Combine( path, String.Format( "Form-{0}.bin", form.Name ) ), FileMode.Create, FileAccess.Write, FileShare.None ) )
+            {
+                formatter.Serialize( bin, form );
+            }
         }
 
-        public static Form Load( string path )
+        public static Form Load( string completePath )
         {
-            throw new NotImplementedException();
+            IFormatter formatter = new BinaryFormatter();
+            using( FileStream bin = new FileStream( completePath, FileMode.Open, FileAccess.Read, FileShare.Read ) )
+            {
+                return (Form)formatter.Deserialize( bin );
+            }
         }
     }
 }
